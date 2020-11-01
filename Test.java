f4e6c9d8d847bcc03fa3f8a19daca17dd7765eb1
qiuyufei <72742566+qiuyufei@users.noreply.github.com>
2020-11-01 11:27:31 +0000

    Update and rename teacher.java to Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+package shiyan3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+		List d=new List();
+		d.show();
+	}
+}
